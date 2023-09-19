deb728c28c7810ceee47bfbd09d00e37ef4d980d
buzzinJohnnyBoi <john_campbell@email.com>
2023-09-18 20:16:00 -0400

    ready to start on machine 3.0.0

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Page from './components/main';
+
+const fs = require('fs');
 
 function App() {
+  const content = fs.readFileSync('C:\\Users\\User\\Documents\\hello.txt', 'utf-8');
+  console.log(content);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Page />
+        {content}
       </header>
     </div>
   );
